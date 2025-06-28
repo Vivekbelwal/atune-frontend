20521e29566aa1a2b72fc28e9899a02afff60d91
Vivek Belwal <vivekbelwal144@gmail.com>
2025-06-29 00:47:58 +0530

    added basic header

--- src/Pages/Home/Home.tsx
+++ src/Pages/Home/Home.tsx
@@ -1,6 +1,10 @@
 import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import "./Home.css";
+import { Layout, Button } from "antd";
+import { Header } from "antd/es/layout/layout";
+import { LogoutOutlined } from "@ant-design/icons";
+import logo from "/logo.svg"; // Using the SVG logo from public folder
 
 function Home() {
   const navigate = useNavigate();
@@ -11,9 +15,33 @@ function Home() {
     }
   }, [navigate]);
 
+  // Handle logout functionality
+  const handleLogout = () => {
+    // Clear all localStorage data
+    localStorage.clear();
+    // Reload the page
+    window.location.reload();
+  };
+
   return (
     <>
-      <div>Home</div>
+      <Layout>
+        <Header className="header">
+          <div className="Logo">
+            <img src={logo} alt="Atune Logo" style={{ height: "40px" }} />
+          </div>
+          <div className="logout">
+            <Button
+              type="primary"
+              danger
+              icon={<LogoutOutlined />}
+              onClick={handleLogout}
+            >
+              Logout
+            </Button>
+          </div>
+        </Header>
+      </Layout>
     </>
   );
 }
